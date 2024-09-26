c28a209eacc989a5eb089ff8735126bcf8b360fd
AlanAl3x <alanalejandro2007@gmail.com>
2024-09-26 17:14:01 -0600

    Chequeo de no repetir info en la base de datos

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -7,12 +7,44 @@
     $usuario = mysqli_real_escape_string($conexion, $_POST['usuario']);
     $contrasena = mysqli_real_escape_string($conexion, $_POST['contrasena']);
 
+    //Encriptamiento de contraseña
+    // $contrasena = hash('sha512', $contrasena);
+
     $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena)
               VALUES ('$nombre_completo', '$correo', '$usuario', '$contrasena')";
 
+               
+    //Verificar que el CORREO no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo'");
+
+    if(mysqli_num_rows($verificar_correo) > 0){
+        echo'
+        <script>
+        alert("El correo ya se encuentra registrado");
+        window.location ="../index.php";
+        </script>
+        ';
+        exit();
+    }
+
+        //Verificar que el USUARIO no se repita en la base de datos
+        $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario'");
+
+        if(mysqli_num_rows($verificar_usuario) > 0){
+            echo'
+            <script>
+            alert("El usuario ya se encuentra registrado");
+            window.location ="../index.php";
+            </script>
+            ';
+            exit();
+        }
+
+
+    
     $ejecutar = mysqli_query($conexion, $query);
 
-    if ($ejecutar) {
+    if ($ejecutar) { //Mensaje de registro
         echo '
             <script>
             alert("Registro exitoso");

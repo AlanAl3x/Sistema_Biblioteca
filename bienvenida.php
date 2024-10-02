cb00b04d4aa9b48fa98190c4f3f7b92177816711
AlanAl3x <alanalejandro2007@gmail.com>
2024-10-02 15:53:45 -0600

    Pagina bienvenida con fondo y saludo

--- bienvenida.php
+++ bienvenida.php
@@ -15,6 +15,27 @@
     }
     //session_destroy(); //Termina la sesion
 
+ // PAGINA BIENVENIDO
+
+        // Incluir la conexión a la base de datos
+        include 'php/conexion_be.php';
+
+        // Obtener el correo del usuario desde la sesión
+        $correo = $_SESSION['usuario'];
+
+        // Consulta para obtener el nombre del usuario usando el correo
+        $query = "SELECT nombre_completo FROM usuarios WHERE correo = '$correo'";
+        $resultado = mysqli_query($conexion, $query);
+
+        // Verificar si la consulta fue exitosa y obtener el nombre
+        if($resultado && mysqli_num_rows($resultado) > 0){
+            $row = mysqli_fetch_assoc($resultado);
+            $nombre_usuario = $row['nombre_completo'];
+           // print_r($nombre_usuario); //impresion nombre_completo del usuario
+        } else {
+            // Si no se encuentra el nombre, manejar el error
+            echo "Error al encontrar el nombre de usuario";
+        }
 
 ?>
 
@@ -23,10 +44,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenida - Nombre</title>
+    <title> Libros de <?php print_r($nombre_usuario); ?></title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">;
+
+    <link rel="stylesheet" href="assets/css/styles_Bienvenida.css">
 </head>
 <body>
-    <h1>Bienvenido al server pa</h1>
-    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+    <main> 
+        <div class="contenedor__inicio-hola">
+            <h1> Hola <?php print_r($nombre_usuario); ?></h1>
+            </div>
+            
+            <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+    </main>
 </body>
 </html>
